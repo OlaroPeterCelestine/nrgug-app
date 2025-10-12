094363e51da418de98b7e32ef371231f6c23bb46
Olaro Peter Celestine <olaropetercelestine@gmail.com>
2025-10-12 14:23:01 +0300

    Add NRG logo favicon
    - Created custom NRG logo favicon with red background and white text
    - Added multiple favicon formats (SVG, ICO, PNG) for better browser support
    - Updated layout.tsx to include all favicon sizes (16x16, 32x32)
    - Added radio wave design elements to represent radio station
    - Replaced generic favicon with branded NRG Radio Uganda logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
         <script
           type="application/ld+json"
